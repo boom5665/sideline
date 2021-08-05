8c5ec8ea4452196c8d625e338975e5be2200fc3e
totolove1126 <to_patipat@hotmail.com>
2021-08-05 21:31:00 +0700

    t

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -55,7 +55,7 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 
-
+    @yield('css')
     
 
 </head>
@@ -63,6 +63,7 @@
 <body>
     @yield('content')
     @yield('js')
+    
 </body>
 
 <footer>
